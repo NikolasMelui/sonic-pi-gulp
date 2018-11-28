1000b0d36eae7a404eee5af4aa02ec3cc950a8b2
nikolasmelui <slak@samaradom.ru>
2018-11-28 21:45:26 +0400

    Remove old files. Add new code.

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,10 @@
 const gulp = require('gulp');
 const { exec } = require('child_process');
 
-gulp.task('music', () => exec('cat ./music.rb | sonic_pi'));
+gulp.task('play', () => exec('cat ./music.rb | sonic_pi'));
 
-gulp.task('watch', () => {
-  gulp.watch('./**/*.rb', { usePolling: true }, gulp.series('music'));
-});
+gulp.task('watch', () =>
+  gulp.watch('./**/*.rb', { usePolling: true }, gulp.series('play'))
+);
 
 gulp.task('default', gulp.series('watch'));
